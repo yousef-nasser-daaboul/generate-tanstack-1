991e84b4fed253f0f71ec7947926ced0bce12fcc
Yousef Daaboul <yousef.d@fikratech.com>
2026-03-12 13:48:14 +0300

    fix url

--- composables/download-module.ts
+++ composables/download-module.ts
@@ -12,14 +12,14 @@ export async function downloadModule(module: string, project: Projects) {
     project === Projects.Sahab
       ? `https://dev.sahabsoft.com/api/Common/ClientCode/GetFile?module=${module}`
       : project === Projects.Dinarak
-        ? `https://dev-dinarak.asasstech.com:443/ClientCode/Get?module=${module}`
+        ? `https://dev-dinarak.asasstech.com:443/Global/ClientCode/Get?module=${module}`
         : project === Projects.Masar
           ? `https://dev-masar.asasstech.com:443/ClientCode/Generate?module=${module}`
           : project === Projects.Bayan
             ? `https://dev-bayan.asasstech.com:443/ClientCode/Generate?module=${module}`
             : project === Projects.AmanPay
               ? `https://dev-amanpay.asasstech.com:443/ClientCode/Generate?module=${module}`
-              : `https://dev-amanpay-business.asasstech.com:443/ClientCode/Generate?module=${module}`
+              : `https://dev-amanpay-business.asasstech.com:443/ClientCode/Generate?module=${module}`,
   );
 
   if (!response.ok) {
